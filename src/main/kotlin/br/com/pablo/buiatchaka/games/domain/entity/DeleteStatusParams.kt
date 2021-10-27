ace66befa75ad90928eb16a6990eb4a08091cce6
Pablo <pabloluiz.ribeiroo@gmail.com>
2021-10-27 18:31:54 -0300

    feature: Implementando conjunto de classes para status de deleção para entidades do sistema

---
+++ src/main/kotlin/br/com/pablo/buiatchaka/games/domain/entity/DeleteStatusParams.kt
@@ -0,0 +1,19 @@
+package br.com.pablo.buiatchaka.games.domain.entity
+
+import br.com.pablo.buiatchaka.games.usecase.exception.InvalidDeleteStatusException
+import org.apache.commons.lang3.StringUtils
+
+enum class DeleteStatusParams(val value: String) {
+    ACTIVE("Ativo"),
+    DISABLED("Excluído");
+
+    companion object {
+        fun fromDeleteStatus(value: String): DeleteStatusParams {
+            return if (StringUtils.isBlank(value)) {
+                throw InvalidDeleteStatusException()
+            } else {
+                values().first { status -> status.value == value }
+            }
+        }
+    }
+}
