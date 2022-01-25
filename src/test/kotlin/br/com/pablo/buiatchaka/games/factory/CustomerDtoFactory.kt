9e67556cbaafbd885da0a0fd0babf7f5031c044f
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-01-25 01:06:01 -0300

    refactor: Adicionando valor default para ID de CustomerDto

--- src/test/kotlin/br/com/pablo/buiatchaka/games/factory/CustomerDtoFactory.kt
+++ src/test/kotlin/br/com/pablo/buiatchaka/games/factory/CustomerDtoFactory.kt
@@ -5,11 +5,12 @@ import br.com.pablo.buiatchaka.games.domain.dto.CustomerDto
 class CustomerDtoFactory {
 
     companion object {
+        private const val ID = 1L
         private const val EMAIL = "josedasilva@gmail.com"
         private const val PASSWORD = "jose123"
     }
 
     fun create(): CustomerDto {
-        return CustomerDto(email = EMAIL, password = PASSWORD)
+        return CustomerDto(id = ID, email = EMAIL, password = PASSWORD)
     }
 }
