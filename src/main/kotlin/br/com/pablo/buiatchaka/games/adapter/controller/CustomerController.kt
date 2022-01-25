77a10f6b48e92005d81d29d2b4ef31cb6d9f38c7
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-01-25 01:05:36 -0300

    test: Implementando testes para find e findALl

--- src/main/kotlin/br/com/pablo/buiatchaka/games/adapter/controller/CustomerController.kt
+++ src/main/kotlin/br/com/pablo/buiatchaka/games/adapter/controller/CustomerController.kt
@@ -4,6 +4,8 @@ import br.com.pablo.buiatchaka.games.adapter.service.CustomerService
 import br.com.pablo.buiatchaka.games.domain.dto.CustomerDto
 import br.com.pablo.buiatchaka.games.domain.entity.Customer
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,5 +20,17 @@ class CustomerController(
     @PostMapping("/login")
     fun login(@RequestBody customerDto: CustomerDto): ResponseEntity<Customer> {
         return ResponseEntity.ok().body(customerService.login(customerDto))
+    }
+
+    @GetMapping("{id}")
+    fun find(@PathVariable id: Long): ResponseEntity<Customer> {
+        val customer = customerService.find(id)
+        return ResponseEntity.ok().body(customer)
+    }
+
+    @GetMapping("")
+    fun findAll(): ResponseEntity<List<Customer>> {
+        val customers = customerService.findAll()
+        return ResponseEntity.ok().body(customers)
     }
 }
--- src/test/kotlin/br/com/pablo/buiatchaka/games/adapter/controller/CustomerControllerTest.kt
+++ src/test/kotlin/br/com/pablo/buiatchaka/games/adapter/controller/CustomerControllerTest.kt
@@ -1,10 +1,12 @@
 package br.com.pablo.buiatchaka.games.adapter.controller
 
 import br.com.pablo.buiatchaka.games.adapter.service.CustomerService
+import br.com.pablo.buiatchaka.games.domain.entity.Customer
 import br.com.pablo.buiatchaka.games.factory.CustomerDtoFactory
 import br.com.pablo.buiatchaka.games.factory.CustomerFactory
 import br.com.pablo.buiatchaka.games.usecase.constant.ONCE
 import br.com.pablo.buiatchaka.games.usecase.exception.InvalidLoginException
+import br.com.pablo.buiatchaka.games.usecase.exception.ObjectNotFoundException
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
@@ -13,6 +15,7 @@ import org.junit.Assert
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
+import java.lang.NullPointerException
 
 @ExtendWith(MockKExtension::class)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
@@ -29,7 +32,6 @@ internal class CustomerControllerTest {
         customerController.login(customerDto)
 
         verify(exactly = ONCE) { customerServiceMock.login(any()) }
-        verify(exactly = ONCE) { customerController.login(any()) }
     }
 
     @Test
@@ -39,6 +41,55 @@ internal class CustomerControllerTest {
         Assert.assertThrows(InvalidLoginException::class.java) { customerController.login(customerDto) }
 
         verify(exactly = ONCE) { customerServiceMock.login(any()) }
-        verify(exactly = ONCE) { customerController.login(any()) }
+    }
+
+    @Test
+    fun mustFindWithSuccess() {
+        every { customerServiceMock.find(any()) } returns customer
+
+        customerController.find(customerDto.id!!)
+
+        verify(exactly = ONCE) { customerServiceMock.find(any()) }
+    }
+
+    @Test
+    fun mustFindWithError() {
+        every { customerServiceMock.find(any()) } throws ObjectNotFoundException(Customer::class.java.simpleName, ONCE.toLong())
+
+        Assert.assertThrows(ObjectNotFoundException::class.java) { customerController.find(customerDto.id!!) }
+
+        verify(exactly = ONCE) { customerServiceMock.find(any()) }
+    }
+
+    @Test
+    fun mustFindAllWithSuccess() {
+        every { customerServiceMock.findAll() } returns listOf(customer)
+
+        customerController.findAll()
+
+        verify(exactly = ONCE) { customerServiceMock.findAll() }
+    }
+
+    @Test
+    fun mustFindAllEmptyListWithSuccess() {
+        every { customerServiceMock.findAll() } returns listOf()
+
+        customerController.findAll()
+
+        verify(exactly = ONCE) { customerServiceMock.findAll() }
+    }
+
+    @Test
+    fun mustFindAllWithError() {
+        every { customerServiceMock.findAll() } throws NullPointerException()
+
+        Assert.assertThrows(NullPointerException::class.java) { customerController.findAll() }
+
+        verify(exactly = ONCE) { customerServiceMock.findAll() }
+    }
+
+    @Test
+    fun mustSaveWithSuccess() {
+        every { customerServiceMock.save(any()) }
     }
 }
