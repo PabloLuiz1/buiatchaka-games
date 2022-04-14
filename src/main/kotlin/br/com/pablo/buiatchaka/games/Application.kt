a9735c33f2e881f20fa66678d178af58fd342798
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-04-13 23:42:38 -0300

    style: Formatando a classe de Application

--- src/main/kotlin/br/com/pablo/buiatchaka/games/Application.kt
+++ src/main/kotlin/br/com/pablo/buiatchaka/games/Application.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class Application
 
-    fun main(args: Array<String>) {
-        runApplication<Application>(*args)
-    }
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
